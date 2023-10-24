674bdf6d867e585b68c81a95093ce16bc62a60ee
Valentin Garcia <vgarciaponasso@gmail.com>
2023-10-24 19:38:02 -0300

    Se elimina archivo prueba.js

--- src/prueba.js
+++
@@ -1,9 +0,0 @@
-import ProductManager from "./ProductManager.js";
-
-async function test (){
-    const productManager = new ProductManager('./Productos.json');
-    const productoObtenido =  await productManager.getProduct();
-    console.log("Productos: ", productoObtenido);
-};
-
-test ();
